c5c4bad33bb4526de92fb743c3e87779c1fbf5f0
keith-Yong <2957831617@qq.com>
2022-10-11 22:51:09 +0800

    定义时间过滤器及主页数据展示

--- src/components/PostList.vue
+++ src/components/PostList.vue
@@ -1,7 +1,7 @@
 <!-- Postlist组件，首页/主页 -->
 <template>
   <!-- 主页的html结构 第一个div是根标签-->
-  <div>
+  <div class="PostList">
     <!--  这里的div是在加载慢的时候套一个加载动画 -->
     <!-- v-if的作用：在isLoading状态的时候加载这个标签，在data函数中定义isLoading的值 -->
     <div class="loading" v-if="isLoading">
@@ -12,12 +12,29 @@
     <div>
       <!-- 一篇篇博客形成了主题列表，所以用ul>li结构 -->
 
-      <ul>
+      <ul class="pics">
         <!--  for循环遍历数组 -->
         <li v-for="post in posts" :key="post.id">
-          <!-- 在页面展示图片列表 -->
+          <!-- 在页面展示图片列表 :是动态绑定什么意思？-->
 
           <img :src="post.author.avatar_url" alt="" />
+          <!-- 为什么要用span标签解释：div跟span最大的区别就是span在一行显示而div换行显示，其实掌握好CSS样式span跟div没什么大的区别，一般都用div布局页面，而span一般包在div里面用的比较多。
+span不用样式控制就会在一行显示，而div需要用到float、width的Style样式控制，他才在一行显示。 -->
+            <!-- 需要做分块处理所以用span    -->
+          <span>
+            <!--  回复量用span标签 -->
+            <span class="reply_count">{{post.reply_count}}</span>
+            <!-- 帖子访问量 -->
+            /{{post.visit_count}}
+          </span>
+          <span>
+            <!-- 帖子标题 -->
+            {{post.title}}
+          </span>
+          <!-- 最终回复时间 使用管道过滤器-->
+          <!-- 过滤器语法：写在大括号内 -->
+               
+          <span>{{post.last_reply_at | formatDate}}</span>
         </li>
       </ul>
     </div>
@@ -27,6 +44,7 @@
 
 <script>
 // export default 向外暴露的成员，可以使用任意变量来接收 这里为什么
+
 export default {
   data() {
     return {
@@ -69,5 +87,23 @@ export default {
 };
 </script>
 
-<style>
+<style scoped>
+    /* 主页背景色设为白色 */
+    .PostList {
+        background-color: #e1e1e1;
+    }
+    /*  头像太大 改小 */
+    .pics img {
+        width:30px;
+        height: 30px;
+    }
+    ul {
+        /* 去除图片列表样式 */
+        list-style: none;
+        width:100%;
+        max-width: 1344px;
+        margin: 0 auto;
+
+    }
+
 </style>
