a99448f3077b1965c46529cc7dc4d9ef1197fb8b
keith-Yong <2957831617@qq.com>
2022-10-14 19:09:41 +0800

    使用computed计算属性限制侧边栏 文章题目的 数量

--- src/components/SlideBar.vue
+++ src/components/SlideBar.vue
@@ -15,15 +15,45 @@
             </router-link>
     <div>作者最近主题</div>
     <ul>
-        <li v-for="list in userinfo.recent_topics" :key="list">
+        <!-- topclimtby5是computed函数 -->
+        <li v-for="list in topclimtby5" :key="list">
             <!-- 只需要显示5条，这里会显示多条 -->
+            <!-- 路由指向文章详情页Artist组件 -->
+            <router-link :to="{
+              name:'post_content',
+              params:{
+                // Artist组件需要的参数包括id和name
+                id:list.id,
+                name:list.author.loginname
+              }
+
+            }">
             {{list.title}}
+        </router-link>
         </li>
     </ul>
 </div>
     <div class="recent_topics">
         <div class="topbar">作者最近回复</div>
         <!-- 头像用路由 和aritcle中的一样，直接复制修改参数即可，参数不同,因为接口不同 -->
+        <ul>
+        <!-- topclimtby5是computed函数 -->
+        <li v-for="list,index in replylimtby5" :key="index">
+            <!-- 只需要显示5条，这里会显示多条 -->
+            <router-link :to="{
+              name:'post_content',
+              params:{
+                // Artist组件需要的参数包括id和name
+                id:list.id,
+                name:list.author.loginname
+              }
+
+            }">
+            {{list.title}}
+        </router-link>
+            
+        </li>
+    </ul>
        
             
         
@@ -38,17 +68,34 @@
         name: "SliderBar",
         data(){
             return {
+                isLoading:false,
                 userinfo:{}  //定义userinfo的数据格式，对象
             }
         },
+        computed:{
+            topclimtby5(){
+                if(this.userinfo.recent_topics){
+                    return this.userinfo.recent_topics.slice(0,5);
+                }else{
+                        return this.userinfo.recent_topics;
+                    }
+                },
+            replylimtby5(){
+                if(this.userinfo.recent_replies){
+                    return this.userinfo.recent_replies.slice(0,5);
+                }else{
+                        return this.userinfo.recent_topics;
+                    }
+                },
 
-        created(){
-            this.isLoading = true; //加载成功之前加载动画
-   
-            this.getData(); // 在页面加载之前获取数据
-        },
+            }
+        ,
+
+        // 用计算属性实现最近回复主题只展示5条数据
+        
+        
         methods:{
-        getData() {
+            getData() {
         //    走的api和userinfo一样
            this.$http.get(`https://cnodejs.org/api/v1/user/${this.$route.params.name}`)
        // then后面加成功的 具体行为
@@ -63,9 +110,20 @@
          //   处理返回失败后的问题
          
          console.log(err);
-       });
-   }}
-    }
+       })
+   }
+},
+    created(){
+            this.isLoading = true; //加载成功之前加载动画
+   
+            this.getData(); // 在页面加载之前获取数据
+        },
+    
+
+}
+
+        
+    
 </script>
 
 <style>
