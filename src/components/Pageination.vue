1b56380ab552fb30622eabed3fb361d7a681da1b
keith-Yong <2957831617@qq.com>
2022-10-16 16:31:04 +0800

    分页html结构

---
+++ src/components/Pageination.vue
@@ -0,0 +1,16 @@
+<!-- 分页组件 -->
+<template>
+    <!-- 定义页码html结构 -->
+<div class="pagination">
+    <button>首页</button>
+    <button>«</button>
+    <button>»</button>
+</div>
+</template>
+<script>
+
+</script>
+
+<style>
+
+</style>
