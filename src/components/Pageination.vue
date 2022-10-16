f6c03ccd57fc93101bf35bd1ea372849f8b43ae2
keith-Yong <2957831617@qq.com>
2022-10-16 17:13:51 +0800

    使用v-for循环生成页码的按钮标签

--- src/components/Pageination.vue
+++ src/components/Pageination.vue
@@ -4,11 +4,24 @@
 <div class="pagination">
     <button>首页</button>
     <button>«</button>
+    
+    <!-- 定义页面数字的按钮 -->
+    <button v-for="btn in pagebtns" :key="btn">
+        {{btn}} 
+    </button>
     <button>»</button>
 </div>
 </template>
 <script>
-    
+    export  default {
+        name:"Pageination",
+        data(){
+            return {
+                // 定义数组 分页展示的页码数字，
+                pagebtns:[1,2,3,4,5,'.....']
+            }
+        }
+    }
 </script>
 
 <style>
